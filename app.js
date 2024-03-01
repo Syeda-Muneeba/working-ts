f8c255b669a5b9e81f1059da081eb2721a801d51
Syeda-Muneeba <160313963+Syeda-Muneeba@users.noreply.github.com>
2024-03-01 21:32:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+//exercise 13
+var transportation = ['civic', 'bike', 'bus', 'suzuki cow wali'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('i would like to own a' + transportation[i]);
+}
